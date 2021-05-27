a3befa50a04d6b0a59df115b2d22e1d5434a657b
zengchzh <825597202@qq.com>
2021-05-27 15:24:34 +0800

    添加screw示例

---
+++ screw/src/main/java/org/zchzh/screw/ScrewApplication.java
@@ -0,0 +1,13 @@
+package org.zchzh.screw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScrewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScrewApplication.class, args);
+    }
+
+}
