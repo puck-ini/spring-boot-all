f2d35c864ca9db63eae63c181943ea3c80f20939
zengchzh <825597202@qq.com>
2021-06-01 14:23:08 +0800

    spring-data-jpa指定repository实现

--- spring-data-jpa/src/main/java/org/zchzh/springdatajpa/SpringDataJpaApplication.java
+++ spring-data-jpa/src/main/java/org/zchzh/springdatajpa/SpringDataJpaApplication.java
@@ -2,7 +2,11 @@ package org.zchzh.springdatajpa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.zchzh.springdatajpa.repository.BaseRepoImpl;
 
+
+@EnableJpaRepositories(repositoryBaseClass = BaseRepoImpl.class)
 @SpringBootApplication
 public class SpringDataJpaApplication {
 
