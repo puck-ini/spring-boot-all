b0091c8a425b46ed8084c3419e266b4cea933f0d
zengchzh <825597202@qq.com>
2021-06-05 23:33:56 +0800

    添加gateway模块

---
+++ gateway/src/main/java/org/zchzh/gateway/GatewayApplication.java
@@ -0,0 +1,17 @@
+package org.zchzh.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zengchzh
+ * @date 2021/6/5
+ */
+
+@SpringBootApplication
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+}
