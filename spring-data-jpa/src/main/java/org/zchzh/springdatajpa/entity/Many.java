3fe12531554fc9583e24f043730fc09ddedc0afe
zengchzh <825597202@qq.com>
2021-07-01 11:15:45 +0800

    spring-data-jpa 模块添加 @OneToMany @ManyToOne 使用

---
+++ spring-data-jpa/src/main/java/org/zchzh/springdatajpa/entity/Many.java
@@ -0,0 +1,24 @@
+package org.zchzh.springdatajpa.entity;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+/**
+ * @author zengchzh
+ * @date 2021/6/27
+ */
+
+@Entity
+@Data
+public class Many {
+
+    @Id
+    private Long id;
+
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    // 不生成外键
+    @JoinColumn(foreignKey = @ForeignKey(ConstraintMode.NO_CONSTRAINT))
+    private One one;
+}
---
+++ spring-data-jpa/src/main/java/org/zchzh/springdatajpa/entity/One.java
@@ -0,0 +1,22 @@
+package org.zchzh.springdatajpa.entity;
+
+import lombok.Data;
+import lombok.ToString;
+
+import javax.persistence.*;
+import java.util.List;
+
+/**
+ * @author zengchzh
+ * @date 2021/6/27
+ */
+@Entity
+@Data
+public class One {
+    @Id
+    private Long id;
+    // 解决 lombok 生成的toString 方法循环调用问题
+    @ToString.Exclude
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "one", fetch = FetchType.EAGER)
+    private List<Many> manies;
+}
