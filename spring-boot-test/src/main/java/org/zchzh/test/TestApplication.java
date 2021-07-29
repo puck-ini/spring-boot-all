23fabda28ee079b6d967de3b91b83ddff3ae120f
zengchzh <825597202@qq.com>
2021-07-29 10:37:54 +0800

    feat(spring-boot-test):初始化模块

---
+++ spring-boot-test/src/main/java/org/zchzh/test/TestApplication.java
@@ -0,0 +1,17 @@
+package org.zchzh.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zengchzh
+ * @date 2021/7/29
+ */
+
+@SpringBootApplication
+public class TestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestApplication.class, args);
+    }
+}
---
+++ spring-boot-test/src/test/java/org/zchzh/test/TestApplicationTests.java
@@ -0,0 +1,16 @@
+package org.zchzh.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * @author zengchzh
+ * @date 2021/7/29
+ */
+@SpringBootTest
+public class TestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
