8159e869c4d715c1b3a3e1b4e7d799beb715fdb9
zengchzh <825597202@qq.com>
2021-08-10 22:22:26 +0800

    feat(spring-logback):添加logback例子

---
+++ spring-logback/src/main/java/org/zchzh/logback/LogbackApplication.java
@@ -0,0 +1,16 @@
+package org.zchzh.logback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zengchzh
+ * @date 2021/8/10
+ */
+
+@SpringBootApplication
+public class LogbackApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LogbackApplication.class, args);
+    }
+}
