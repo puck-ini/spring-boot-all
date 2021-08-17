546d4a385deb3e2dced619a293d1910310be842e
zengchzh <825597202@qq.com>
2021-08-17 15:14:37 +0800

    feat(gateway):添加gateway swagger 的配置，可以通过 gateway swagger 访问不同应用的 swagger

---
+++ gateway/src/main/java/org/zchzh/gateway/config/SwaggerResourcesConfig.java
@@ -0,0 +1,74 @@
+package org.zchzh.gateway.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+import springfox.documentation.swagger.web.SwaggerResource;
+import springfox.documentation.swagger.web.SwaggerResourcesProvider;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * @author zengchzh
+ * @date 2021/1/14
+ *
+ * 通过 gateway swagger 访问不同应用的 swagger
+ */
+
+@Primary
+@Component
+public class SwaggerResourcesConfig implements SwaggerResourcesProvider {
+    /**
+     * swagger2默认的url后缀
+     */
+    private static final String SWAGGER2URL = "/v3/api-docs";
+
+    /**
+     * 网关路由
+     */
+    private final RouteLocator routeLocator;
+
+    /**
+     * 网关应用名称
+     */
+    @Value("${spring.application.name}")
+    private String self;
+
+    @Autowired
+    public SwaggerResourcesConfig(RouteLocator routeLocator) {
+        this.routeLocator = routeLocator;
+    }
+
+    @Override
+    public List<SwaggerResource> get() {
+        List<SwaggerResource> resources = new ArrayList<>();
+        List<String> routeHosts = new ArrayList<>();
+        // 获取所有可用的host：serviceId
+        routeLocator.getRoutes().filter(route -> route.getUri().getHost() != null)
+                .filter(route -> !self.equals(route.getUri().getHost()))
+                .subscribe(route -> routeHosts.add(route.getUri().getHost()));
+
+        // 记录已经添加过的server，存在同一个应用注册了多个服务在eureka上
+        Set<String> dealed = new HashSet<>();
+        routeHosts.forEach(instance -> {
+            String[] hostArr = instance.split("-");
+            // 拼接url
+            String hostName = hostArr[0] + "-" + hostArr[1];
+//            String hostName = instance;
+            String url = "/" + hostName + SWAGGER2URL;
+            if (!dealed.contains(url)) {
+                dealed.add(url);
+                SwaggerResource swaggerResource = new SwaggerResource();
+                swaggerResource.setUrl(url);
+                swaggerResource.setName(hostName);
+                resources.add(swaggerResource);
+            }
+        });
+        return resources;
+    }
+}
