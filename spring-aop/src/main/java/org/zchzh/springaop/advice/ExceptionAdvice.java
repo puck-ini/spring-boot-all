ae89abd433d2f842ace3510e7edf60b976a81488
zengchzh <825597202@qq.com>
2022-03-15 09:39:52 +0800

    feat(spring-aop):统一处理异常将http状态修改成500

--- spring-aop/src/main/java/org/zchzh/springaop/advice/ExceptionAdvice.java
+++ spring-aop/src/main/java/org/zchzh/springaop/advice/ExceptionAdvice.java
@@ -1,6 +1,8 @@
 package org.zchzh.springaop.advice;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -15,8 +17,9 @@ import java.util.Date;
 public class ExceptionAdvice {
 
     @ExceptionHandler(Exception.class)
-    public String handle(Exception e) {
+    public Object handle(Exception e) {
         log.info(">>>>>>>> log exception - {}", new Date());
-        return "advice - " + e.getMessage();
+        // 通过这种方式在发生异常时将 http 请求的状态修改成500
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("advice - " + e.getMessage());
     }
 }
