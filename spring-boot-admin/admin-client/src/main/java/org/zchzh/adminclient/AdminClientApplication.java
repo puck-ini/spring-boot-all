3dff241aad7614890e2756a352184a645fea8f43
zengchzh <825597202@qq.com>
2022-06-01 10:41:40 +0800

    feat(spring-boot-admin):添加spring-boot-admin使用

---
+++ spring-boot-admin/admin-client/src/main/java/org/zchzh/adminclient/AdminClientApplication.java
@@ -0,0 +1,17 @@
+package org.zchzh.adminclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zengchzh
+ * @date 2022/5/31
+ */
+
+@SpringBootApplication
+public class AdminClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdminClientApplication.class, args);
+    }
+}
---
+++ spring-boot-admin/admin-server/src/main/java/org/zchzh/adminserver/AdminServerApplication.java
@@ -0,0 +1,17 @@
+package org.zchzh.adminserver;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zengchzh
+ * @date 2022/5/31
+ */
+@EnableAdminServer
+@SpringBootApplication
+public class AdminServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AdminServerApplication.class, args);
+    }
+}
